9bf6f025fb636e6193b71bddc7637ce93686bbc5
molinarijess <lacerdajess@gmail.com>
2022-11-02 19:32:06 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -27,45 +27,29 @@ function writeToLog(
   console.log(logEntries);
 }
 
-function calculateResult(calculationType) {
+function calculateResult(operation) {
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
   let mathOperator;
-  if (calculationType === "ADD") {
+  if (operation === "ADD") {
     currentResult += enteredNumber;
     mathOperator = "+";
-  } else if (calculationType === "SUBTRACT") {
+  } else if (operation === "SUBTRACT") {
     currentResult -= enteredNumber;
     mathOperator = "-";
-  } else if (calculationType === "MULTIPLY") {
+  } else if (operation === "MULTIPLY") {
     currentResult *= enteredNumber;
     mathOperator = "*";
-  } else if (calculationType === "DIVIDE") {
+  } else if (operation === "DIVIDE") {
     currentResult /= enteredNumber;
     mathOperator = "/";
   }
 
   createAndWriteOutput(mathOperator, initialResult, enteredNumber);
-  writeToLog(calculationType, initialResult, enteredNumber, currentResult);
+  writeToLog(operation, initialResult, enteredNumber, currentResult);
 }
 
-function add() {
-  calculateResult("ADD");
-}
-
-function subtract() {
-  calculateResult("SUBTRACT");
-}
-
-function multiply() {
-  calculateResult("MULTIPLY");
-}
-
-function divide() {
-  calculateResult("DIVIDE");
-}
-
-addBtn.addEventListener("click", add);
-subtractBtn.addEventListener("click", subtract);
-multiplyBtn.addEventListener("click", multiply);
-divideBtn.addEventListener("click", divide);
+addBtn.addEventListener("click", calculateResult.bind(this, "ADD"));
+subtractBtn.addEventListener("click", calculateResult.bind(this, "SUBTRACT"));
+multiplyBtn.addEventListener("click", calculateResult.bind(this, "MULTIPLY"));
+divideBtn.addEventListener("click", calculateResult.bind(this, "DIVIDE"));
